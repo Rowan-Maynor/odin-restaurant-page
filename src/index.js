097b0cf298f80690127753c2a34ce1a0c80ae5ec
rowan-maynor <rowanmaynor@gmail.com>
2024-12-10 15:16:14 -0500

    Generate nav options with js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import { createA } from "./library";
 const nav = document.querySelector("nav");
 
 createA("", nav, "append", "nav-home", "nav-option", "Home");
+createA("", nav, "append", "nav-menu", "nav-option", "Menu");
+createA("", nav, "append", "nav-about-us", "nav-option", "About Us");
