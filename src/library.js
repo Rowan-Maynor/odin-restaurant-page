6d122df762f10bd559b46c692ad75d3823d49e28
rowan-maynor <rowanmaynor@gmail.com>
2024-12-10 15:26:25 -0500

    User more logical variable names
    parent and attachType are much clearer on purpose

--- src/library.js
+++ src/library.js
@@ -1,6 +1,6 @@
 export { createImg, createA };
 
-function createImg(src, target, type, id, classList) {
+function createImg(src, parent, attachType, id, classList) {
     const img = document.createElement("img");
     img.src = src;
     if (id) {
@@ -9,14 +9,14 @@ function createImg(src, target, type, id, classList) {
     if (classList) {
         img.classList = classList
     }
-    if (type == "append") {
-        target.append(img);
-    } else if (type == "prepend") {
-        target.prepend(img);
+    if (attachType == "append") {
+        parent.append(img);
+    } else if (attachType == "prepend") {
+        parent.prepend(img);
     }
 }
 
-function createA(href, target, type, id, classList, content) {
+function createA(href, parent, attachType, id, classList, content) {
     const a = document.createElement("a");
     a.href = href;
     if (id) {
@@ -28,14 +28,14 @@ function createA(href, target, type, id, classList, content) {
     if (content) {
         a.textContent = content
     }
-    if (type == "append") {
-        target.append(a);
-    } else if (type == "prepend") {
-        target.prepend(a);
+    if (attachType == "append") {
+        parent.append(a);
+    } else if (attachType == "prepend") {
+        parent.prepend(a);
     }
 }
 
-function createBtn(parent, type, id, classList, content) {
+function createBtn(parent, attachType, id, classList, content) {
     const btn = document.createElement("button");
     if (id) {
         btn.id = id
@@ -46,9 +46,9 @@ function createBtn(parent, type, id, classList, content) {
     if (content) {
         btn.textContent = content
     }
-    if (type == "append") {
+    if (attachType == "append") {
         parent.append(btn);
-    } else if (type == "prepend") {
+    } else if (attachType == "prepend") {
         parent.prepend(btn);
     }
 }
