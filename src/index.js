f8d1dab139109fbb32eba755d023ea695df1a2cb
rowan-maynor <rowanmaynor@gmail.com>
2024-12-13 16:42:14 -0500

    Add a returning animation
    Needed to add the animation with js.
    This is because it would do the animation on
    page load.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+addNavButtonAnimation();
+
+function addNavButtonAnimation() {
+    const navButton = document.querySelectorAll(".nav-option");
+    navButton.forEach(button => {
+        button.addEventListener("mouseover", function () {
+            button.classList = "nav-option nav-animation";
+        }, {once :true});
+    });
+}
