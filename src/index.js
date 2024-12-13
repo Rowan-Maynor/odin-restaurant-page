65c0be5b7e596cb8902d2abdfd1afb2ee60f5cf5
rowan-maynor <rowanmaynor@gmail.com>
2024-12-13 15:28:02 -0500

    Hard code buttons to template instead of using js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,3 @@ import "./styles.css";
 import { createBtn } from "./library";
 
 const nav = document.querySelector("nav");
-
-createBtn(nav, "append", "nav-home", "nav-option", "Home");
-createBtn(nav, "append", "nav-menu", "nav-option", "Menu");
-createBtn(nav, "append", "nav-about-us", "nav-option", "About Us");
