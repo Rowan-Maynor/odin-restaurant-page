73678871343ec122e36a478e633acfdabf45fa2a
rowan-maynor <rowanmaynor@gmail.com>
2024-12-17 07:20:55 -0500

    Create dummy data for the hours table

--- src/home.js
+++ src/home.js
@@ -6,6 +6,12 @@ function generateHomePage() {
     createP(content, "append", "home-tag", "", "Come dine in!");
     createP(content, "append", "hours-p", "", "Hours");
     createDiv(content, "append", "hours-table", "grid");
+    const hoursTable = document.querySelector("#hours-table");
+    createP(hoursTable, "append", "sunday", "", "Day");
+    createP(hoursTable, "append", "sunday-hours", "", "Time");
+    createP(hoursTable, "append", "monday", "", "Day");
+    createP(hoursTable, "append", "monday-hours", "", "Time");
+
 }
 
 export { generateHomePage }
