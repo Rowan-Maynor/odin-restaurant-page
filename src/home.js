62a3819d98ecd5f547fa015a0075ff490e62d359
rowan-maynor <rowanmaynor@gmail.com>
2024-12-17 07:07:44 -0500

    Create hours-p under home-tag

--- src/home.js
+++ src/home.js
@@ -4,6 +4,7 @@ const content = document.querySelector("#content");
 
 function generateHomePage() {
     createP(content, "append", "home-tag", "", "Come dine in!");
+    createP(content, "append", "hours-p", "", "Hours");
 }
 
 export { generateHomePage }
