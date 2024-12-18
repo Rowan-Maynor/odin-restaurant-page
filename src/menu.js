e4a2fab4eb05ef73f398f40c609cd77d33712ff5
rowan-maynor <rowanmaynor@gmail.com>
2024-12-18 09:44:21 -0500

    Fix typo

--- src/menu.js
+++ src/menu.js
@@ -14,7 +14,7 @@ function generateMenuPage() {
          "https://images.unsplash.com/photo-1626920370508-cf4d8f916448?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
          "menu-item-1",
         "Sandwich", "$14.00", 
-        "An eggs and Bacon sandwich on a corrisant bun. Contains dairy products.");
+        "An eggs and bacon sandwich on a corrisant bun. Contains dairy products.");
 }
 
 function createMenuItem(parent, src, id, name, price, description) {
