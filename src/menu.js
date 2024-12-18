edaf7f10162122893e40866fd999e116ceb4029d
rowan-maynor <rowanmaynor@gmail.com>
2024-12-18 09:00:55 -0500

    Add an id to help style the image if need be

--- src/menu.js
+++ src/menu.js
@@ -10,13 +10,13 @@ function generateMenuPage() {
     createDiv(content, "append", "menu-container", "flex-container");
     const menuContainer = document.querySelector("#menu-container");
 
-    createMenuItem(menuContainer, "https://images.unsplash.com/photo-1626920370508-cf4d8f916448?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-
-
+    createMenuItem(menuContainer,
+         "https://images.unsplash.com/photo-1626920370508-cf4d8f916448?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+         "menu-item-1");
 }
 
-function createMenuItem(parent, src) {
-    createImg(src, parent, "append", "", "menu-img");
+function createMenuItem(parent, src, id) {
+    createImg(src, parent, "append", id, "menu-img");
 }
 
 export { generateMenuPage }
