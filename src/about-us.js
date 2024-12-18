b49231461928aefd7e44f47987a96089a6b8dc5a
rowan-maynor <rowanmaynor@gmail.com>
2024-12-18 10:48:32 -0500

    Create about-us.js

---
+++ src/about-us.js
@@ -0,0 +1,10 @@
+import { createP, createDiv, removeChildren } from "./library.js"
+
+const content = document.querySelector("#content");
+
+function generateAboutUs() {
+    //clear initial content
+    removeChildren(content);
+
+    
+}
